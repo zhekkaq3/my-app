3785521112e1c524e4c2fa9f7c89320ae99aa1c3
Zhekka <maratzhen@gmail.com>
2024-04-15 13:21:33 +0300

    small ref

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,8 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} **/
 const nextConfig = {
     images: {
         domains: ['encrypted-tbn2.gstatic.com','encrypted-tbn0.gstatic.com','encrypted-tbn3.gstatic.com'],
     },
-    async headers() {
-        return [
-          {
-            source: '/:path*',
-            headers: [
-             {
-               key: 'Content-Security-Policy',
-               value: "upgrade-insecure-requests"
-             }
-            ],
-          },
-        ]
-      },
-    
 };
 
 export default nextConfig;
