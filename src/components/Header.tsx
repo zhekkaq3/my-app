0befbe0b4b2a6eb429931d90bd851f6e68bf347a
Zhekka <maratzhen@gmail.com>
2024-04-15 13:15:27 +0300

    small ref

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="flex flex-col items-center justify-between w-full">
             <h1 className="text-2xl md:text-6xl sm:text-4xl lg:text-8xl font-normal flex items-center justify-center text-white bg-[#777777] rounded-2xl w-full mb-[40px] py-3">
-                тестовое задание
+                тестовое задание!
             </h1>
             <div className="flex items-center justify-between max-w-[975px] w-full">
                 <h3 className="tracking-wide text-2xl ">
